98028cc11a326c7a089c732391ecf24cf8143a94
engineer-kufre <engineer_kufre@yahoo.com>
2020-02-03 20:40:27 +0100

    updated js

--- calc.js
+++ calc.js
@@ -26,7 +26,7 @@ const equal_button = document.getElementById('equal');
 const dayMode_button = document.getElementById('day');
 const darkMode_button = document.getElementById('dark');
 
-let str = '09876';
+let str = '';
 
 darkMode_button.addEventListener('click', function () {
     document.getElementById('body').classList.add('night');
@@ -35,10 +35,12 @@ dayMode_button.addEventListener('click', function () {
     document.getElementById('body').classList.remove('night');
 })
 back_button.addEventListener('click', function () {
-    result_p.innerHTML = str.split('').slice(0, str.length - 1).join('');
+    str = str.split('').slice(0, str.length - 1).join('');
+    result_p.innerHTML = str;
 })
 cancel_button.addEventListener('click', function () {
-    result_p.innerHTML = 0;
+    str = 0;
+    result_p.innerHTML = str;
 })
 plusminus_button.addEventListener('click', function () {
     if (str[0] === '-') {
@@ -55,4 +57,86 @@ sqrt_button.addEventListener('click', function () {
     } else {
         result_p.innerHTML = parseFloat(Math.sqrt(Number(str)).toFixed(12));
     }
+})
+seven_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '7';
+    result_p.innerHTML = str;
+})
+eight_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '8';
+    result_p.innerHTML = str;
+})
+nine_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '9';
+    result_p.innerHTML = str;
+})
+four_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '4';
+    result_p.innerHTML = str;
+})
+five_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '5';
+    result_p.innerHTML = str;
+})
+six_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '6';
+    result_p.innerHTML = str;
+})
+one_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '1';
+    result_p.innerHTML = str;
+})
+two_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '2';
+    result_p.innerHTML = str;
+})
+three_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '3';
+    result_p.innerHTML = str;
+})
+zero_button.addEventListener('click', function () {
+    if (str === 0) {
+        str = '';
+    }
+    str += '0';
+    result_p.innerHTML = str;
+})
+point_button.addEventListener('click', function () {
+    if (str === 0 || str === '0') {
+        str = '0.';
+    }
+    else if (!str.includes('.')) {
+        str += '.';
+    }
+})
+inverse_button.addEventListener('click', function () {
+    result_p.innerHTML = parseFloat((1 / Number(str)).toFixed(12));
+    
 })
